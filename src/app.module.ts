8a943cb99a17fdb4dde7ae94929fb960e7315fe9
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-01-22 13:32:36 -0300

    create : resource academic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AcademicModule } from './academic/academic.module';
 
 @Module({
-  imports: [],
+  imports: [AcademicModule],
   controllers: [AppController],
   providers: [AppService],
 })
